90706fad5b9b06a3604a64753c48df50bbdb6259
skewkelp <skewkelp@gmail.com>
2024-11-26 18:58:19 +0800

    UPDATE: db sql

--- classes/room-status.class.php
+++ classes/room-status.class.php
@@ -69,41 +69,55 @@ class Room
     }
     
     function showAllRooms() {
-        $sql = 
-        "SELECT r.id, 
-               r.room_name, 
-               CONCAT(rt.room_code, '-', rt.room_description) AS room_details,
-               (SELECT sl.subject_code 
-                FROM class c 
-                LEFT JOIN subject_list sl ON c.subject_id = sl.id 
-                WHERE c.room_id = r.id) AS subject_code,
-               (SELECT cl.section_name 
-                FROM class c 
-                LEFT JOIN class_list cl ON c.class_name = cl.section_name 
-                WHERE c.room_id = r.id) AS section,  
-               (SELECT cs.start_time 
-                FROM class_schedule cs 
-                LEFT JOIN class c ON cs.class_id = c.id 
-                WHERE c.room_id = r.id) AS start_time,
-               (SELECT cs.end_time 
-                FROM class_schedule cs 
-                LEFT JOIN class c ON cs.class_id = c.id 
-                WHERE c.room_id = r.id) AS end_time,
-               (SELECT CONCAT(fl.fname, ' ', fl.lname) AS teacher_name 
-                FROM class c 
-                LEFT JOIN faculty_list fl ON fl.teacher_assigned = c.id 
-                WHERE c.room_id = r.id) AS assigned,
-               (SELECT a.status 
-                FROM room_availability a 
-                LEFT JOIN room_list r ON a.room_id = r.id 
-                WHERE r.id = r.id) AS status
+        // $sql = 
+        // "SELECT r.id, 
+        //        r.room_name, 
+        //        CONCAT(rt.room_code, '-', rt.room_description) AS room_details,
+        //        (SELECT sl.subject_code 
+        //         FROM class c 
+        //         LEFT JOIN subject_list sl ON c.subject_id = sl.id 
+        //         WHERE c.room_id = r.id) AS subject_code,
+        //        (SELECT cl.section_name 
+        //         FROM class c 
+        //         LEFT JOIN class_list cl ON c.class_name = cl.section_name 
+        //         WHERE c.room_id = r.id) AS section,  
+        //        (SELECT cs.start_time 
+        //         FROM class_schedule cs 
+        //         LEFT JOIN class c ON cs.class_id = c.id 
+        //         WHERE c.room_id = r.id) AS start_time,
+        //        (SELECT cs.end_time 
+        //         FROM class_schedule cs 
+        //         LEFT JOIN class c ON cs.class_id = c.id 
+        //         WHERE c.room_id = r.id) AS end_time,
+        //        (SELECT CONCAT(fl.fname, ' ', fl.lname) AS teacher_name 
+        //         FROM class c 
+        //         LEFT JOIN faculty_list fl ON fl.teacher_assigned = c.id 
+        //         WHERE c.room_id = r.id) AS assigned,
+        //        (SELECT a.status 
+        //         FROM room_availability a 
+        //         LEFT JOIN room_list r ON a.room_id = r.id 
+        //         WHERE r.id = r.id) AS status
     
-        FROM room_list r
-        LEFT JOIN room_type rt ON r.room_type = rt.room_code
+        // FROM room_list r
+        // LEFT JOIN room_type rt ON r.room_type = rt.room_code
         
-        WHERE (CONCAT(r.room_type, ' ', r.room_no) LIKE CONCAT('%', :room_name, '%')) 
-        AND (:room_type = '' OR rt.room_code = :room_type);
-        ";
+        // WHERE (CONCAT(r.room_type, ' ', r.room_no) LIKE CONCAT('%', :room_name, '%')) 
+        // AND (:room_type = '' OR rt.room_code = :room_type);
+        // ";
+
+        $sql = "SELECT cda.week_day, 
+            FROM class_day cda LEFT JOIN class_time ct ON cda.class_id = ct.id
+            LEFT JOIN class_details cdt ON ct.class_id = cdt.id
+            
+            
+            UNION
+            (SELECT * FROM class_details cd LEFT JOIN room_list rl ON cd.room_id = rl.id)
+            
+            
+        
+        
+        
+        ;";
     
         // Prepare and execute the statement with PDO
         $stmt = $pdo->prepare($sql);
--- room_utilization.sql
+++ room_utilization.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2024 at 10:09 AM
+-- Generation Time: Nov 26, 2024 at 11:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `account` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('student','teacher','admin') NOT NULL,
@@ -44,7 +44,9 @@ CREATE TABLE `account` (
 
 INSERT INTO `account` (`id`, `first_name`, `last_name`, `username`, `password`, `role`, `is_staff`, `is_admin`) VALUES
 (1, 'Rhamirl', 'Jaafar', 'rham', '$2y$10$42jGvgsU9zoKdFwm18wqQe0nzO78jZvk1m1vMvx8BJYsLTHPuBaCa', 'admin', 1, 1),
-(2, 'aziz', 'amin', 'amin123', '$2y$10$snPP2mhvYzbHhLYvQNqtcODt/ISqzoVkxK/THlbFFn26SFZ9yOT7u', 'student', 1, 1);
+(2, 'aziz', 'amin', 'amin123', '$2y$10$snPP2mhvYzbHhLYvQNqtcODt/ISqzoVkxK/THlbFFn26SFZ9yOT7u', 'student', 1, 1),
+(5, 'first', 'teacher', 'teacher1', '$2y$10$mvrv6cbnYmTAwXO.lK6UxevwEWqjCdk5bSMi1EJVt.8P2fD0BGMXO', 'teacher', 1, 0),
+(6, 'second', 'teacher', 'teacher2', '$2y$10$cInqcsJW1Wh6mqDji3OO1eXC6nEtUE0AF6D3Ha1XPn0qdYbRD..S.', 'teacher', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -68,16 +70,44 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `class`
+-- Table structure for table `class_day`
 --
 
-CREATE TABLE `class` (
+CREATE TABLE `class_day` (
+  `id` int(11) NOT NULL,
+  `week_day` enum('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','') NOT NULL,
+  `class_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `class_day`
+--
+
+INSERT INTO `class_day` (`id`, `week_day`, `class_id`) VALUES
+(1, 'Monday', 5),
+(2, 'Monday', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_details`
+--
+
+CREATE TABLE `class_details` (
   `id` int(11) NOT NULL,
   `class_name` varchar(255) NOT NULL,
   `room_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL,
   `teacher_assigned` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `class_details`
+--
+
+INSERT INTO `class_details` (`id`, `class_name`, `room_id`, `subject_id`, `teacher_assigned`) VALUES
+(8, 'CS1A', 1, 1, 1),
+(9, 'CS1A', 1, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -87,7 +117,7 @@ CREATE TABLE `class` (
 
 CREATE TABLE `class_list` (
   `id` int(11) NOT NULL,
-  `class_name` varchar(255) NOT NULL,
+  `section_name` varchar(255) NOT NULL,
   `course` enum('BSCS','BSIT','ACT') NOT NULL,
   `year_level` enum('1','2','3','4','5') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -96,7 +126,7 @@ CREATE TABLE `class_list` (
 -- Dumping data for table `class_list`
 --
 
-INSERT INTO `class_list` (`id`, `class_name`, `course`, `year_level`) VALUES
+INSERT INTO `class_list` (`id`, `section_name`, `course`, `year_level`) VALUES
 (1, 'CS1A', 'BSCS', '1'),
 (2, 'CS1B', 'BSCS', '1'),
 (3, 'CS1C', 'BSCS', '1'),
@@ -108,17 +138,45 @@ INSERT INTO `class_list` (`id`, `class_name`, `course`, `year_level`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `class_schedule`
+-- Table structure for table `class_time`
 --
 
-CREATE TABLE `class_schedule` (
+CREATE TABLE `class_time` (
   `id` int(11) NOT NULL,
   `class_id` int(11) NOT NULL,
-  `day` enum('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','') NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   `time_modified` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `class_time`
+--
+
+INSERT INTO `class_time` (`id`, `class_id`, `start_time`, `end_time`, `time_modified`) VALUES
+(5, 8, '07:00:00', '09:00:00', '2024-11-26 10:32:46'),
+(6, 9, '09:00:00', '12:00:00', '2024-11-26 10:32:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty_list`
+--
+
+CREATE TABLE `faculty_list` (
+  `id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `faculty_list`
+--
+
+INSERT INTO `faculty_list` (`id`, `account_id`, `fname`, `lname`) VALUES
+(1, 6, 'first', 'teacher'),
+(2, 6, 'second', 'teacher');
 
 -- --------------------------------------------------------
 
@@ -147,19 +205,6 @@ INSERT INTO `product` (`id`, `code`, `name`, `category_id`, `price`, `created_at
 -- --------------------------------------------------------
 
 --
--- Table structure for table `room_availability`
---
-
-CREATE TABLE `room_availability` (
-  `id` int(11) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `time_modified` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `room_list`
 --
 
@@ -179,6 +224,19 @@ CREATE TABLE `room_list` (
 INSERT INTO `room_list` (`id`, `room_name`, `room_type`, `room_no`, `created_at`, `updated_at`) VALUES
 (1, 'LR 1', 'LR', '1', '2024-11-17 14:09:21', '2024-11-17 14:09:21'),
 (2, 'LR 2', 'LR', '2', '2024-11-17 14:09:56', '2024-11-17 14:09:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_status`
+--
+
+CREATE TABLE `room_status` (
+  `id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `time_modified` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -247,15 +305,26 @@ INSERT INTO `stocks` (`id`, `product_id`, `quantity`, `status`, `reason`, `creat
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subject_list`
+-- Table structure for table `subject_details`
 --
 
-CREATE TABLE `subject_list` (
+CREATE TABLE `subject_details` (
   `id` int(11) NOT NULL,
-  `subject_code` varchar(255) NOT NULL,
+  `subject_code` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
   `subject_type` enum('LEC','LAB') NOT NULL,
-  `time_modified` timestamp NOT NULL DEFAULT current_timestamp()
+  `time_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subject_details`
+--
+
+INSERT INTO `subject_details` (`id`, `subject_code`, `description`, `subject_type`, `time_modified`) VALUES
+(1, 'CC103', 'Data Structures and Algorithm', 'LEC', '2024-11-26 09:57:51'),
+(2, 'CC103', 'Data Structures and Algorithm', 'LAB', '2024-11-26 09:57:51'),
+(3, 'CC100', 'Introduction to Computing Studies', 'LEC', '2024-11-26 09:58:26'),
+(4, 'CC100', 'Introduction to Computing Studies', 'LAB', '2024-11-26 09:58:26');
 
 --
 -- Indexes for dumped tables
@@ -265,7 +334,9 @@ CREATE TABLE `subject_list` (
 -- Indexes for table `account`
 --
 ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `first_name` (`first_name`,`last_name`) USING BTREE;
 
 --
 -- Indexes for table `category`
@@ -274,27 +345,43 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `class`
+-- Indexes for table `class_day`
 --
-ALTER TABLE `class`
+ALTER TABLE `class_day`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `classtimeid_fk` (`class_id`);
+
+--
+-- Indexes for table `class_details`
+--
+ALTER TABLE `class_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subject_id` (`subject_id`),
+  ADD KEY `facultyid_fk` (`teacher_assigned`),
   ADD KEY `classname_fk` (`class_name`),
-  ADD KEY `classroomid_fk` (`room_id`),
-  ADD KEY `subjectid_fk` (`subject_id`);
+  ADD KEY `classroomid_fk` (`room_id`);
 
 --
 -- Indexes for table `class_list`
 --
 ALTER TABLE `class_list`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `class_name` (`class_name`);
+  ADD UNIQUE KEY `class_name` (`section_name`);
 
 --
--- Indexes for table `class_schedule`
+-- Indexes for table `class_time`
 --
-ALTER TABLE `class_schedule`
+ALTER TABLE `class_time`
   ADD PRIMARY KEY (`id`),
   ADD KEY `classid_fk` (`class_id`);
+
+--
+-- Indexes for table `faculty_list`
+--
+ALTER TABLE `faculty_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `accountid_fk` (`account_id`),
+  ADD KEY `name` (`fname`,`lname`) USING BTREE;
 
 --
 -- Indexes for table `product`
@@ -305,14 +392,6 @@ ALTER TABLE `product`
   ADD KEY `cat_idfk` (`category_id`);
 
 --
--- Indexes for table `room_availability`
---
-ALTER TABLE `room_availability`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `roomid_fk` (`room_id`),
-  ADD KEY `status_fk` (`status`);
-
---
 -- Indexes for table `room_list`
 --
 ALTER TABLE `room_list`
@@ -320,6 +399,14 @@ ALTER TABLE `room_list`
   ADD UNIQUE KEY `room_no` (`room_no`),
   ADD UNIQUE KEY `room_name` (`room_name`),
   ADD KEY `roomtype_fk` (`room_type`);
+
+--
+-- Indexes for table `room_status`
+--
+ALTER TABLE `room_status`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status_fk` (`status`),
+  ADD KEY `status_roomid_fk` (`room_id`);
 
 --
 -- Indexes for table `room_type`
@@ -343,10 +430,11 @@ ALTER TABLE `stocks`
   ADD KEY `productid_fk` (`product_id`);
 
 --
--- Indexes for table `subject_list`
+-- Indexes for table `subject_details`
 --
-ALTER TABLE `subject_list`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `subject_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subject_details` (`subject_code`,`subject_type`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -356,7 +444,7 @@ ALTER TABLE `subject_list`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -365,10 +453,16 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `class`
+-- AUTO_INCREMENT for table `class_day`
 --
-ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `class_day`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `class_details`
+--
+ALTER TABLE `class_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `class_list`
@@ -377,10 +471,16 @@ ALTER TABLE `class_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `class_schedule`
+-- AUTO_INCREMENT for table `class_time`
 --
-ALTER TABLE `class_schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `class_time`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `faculty_list`
+--
+ALTER TABLE `faculty_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -389,16 +489,16 @@ ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `room_availability`
---
-ALTER TABLE `room_availability`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `room_list`
 --
 ALTER TABLE `room_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `room_status`
+--
+ALTER TABLE `room_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `room_type`
@@ -413,28 +513,42 @@ ALTER TABLE `stocks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `subject_list`
+-- AUTO_INCREMENT for table `subject_details`
 --
-ALTER TABLE `subject_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `subject_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `class`
+-- Constraints for table `class_day`
 --
-ALTER TABLE `class`
-  ADD CONSTRAINT `classname_fk` FOREIGN KEY (`class_name`) REFERENCES `class_list` (`class_name`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `classroomid_fk` FOREIGN KEY (`room_id`) REFERENCES `room_list` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `subjectid_fk` FOREIGN KEY (`subject_id`) REFERENCES `subject_list` (`id`) ON UPDATE CASCADE;
+ALTER TABLE `class_day`
+  ADD CONSTRAINT `classtimeid_fk` FOREIGN KEY (`class_id`) REFERENCES `class_time` (`id`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `class_schedule`
+-- Constraints for table `class_details`
 --
-ALTER TABLE `class_schedule`
-  ADD CONSTRAINT `classid_fk` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON UPDATE CASCADE;
+ALTER TABLE `class_details`
+  ADD CONSTRAINT `classname_fk` FOREIGN KEY (`class_name`) REFERENCES `class_list` (`section_name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `classroomid_fk` FOREIGN KEY (`room_id`) REFERENCES `room_list` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `facultyid_fk` FOREIGN KEY (`teacher_assigned`) REFERENCES `faculty_list` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `subjectid_fk` FOREIGN KEY (`subject_id`) REFERENCES `subject_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `class_time`
+--
+ALTER TABLE `class_time`
+  ADD CONSTRAINT `classid_fk` FOREIGN KEY (`class_id`) REFERENCES `class_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `faculty_list`
+--
+ALTER TABLE `faculty_list`
+  ADD CONSTRAINT `accountid_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `name_fk` FOREIGN KEY (`fname`,`lname`) REFERENCES `account` (`first_name`, `last_name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `product`
@@ -443,17 +557,17 @@ ALTER TABLE `product`
   ADD CONSTRAINT `cat_idfk` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 --
--- Constraints for table `room_availability`
---
-ALTER TABLE `room_availability`
-  ADD CONSTRAINT `roomid_fk` FOREIGN KEY (`room_id`) REFERENCES `room_list` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `status_fk` FOREIGN KEY (`status`) REFERENCES `status` (`status`) ON UPDATE CASCADE;
-
---
 -- Constraints for table `room_list`
 --
 ALTER TABLE `room_list`
   ADD CONSTRAINT `roomtype_fk` FOREIGN KEY (`room_type`) REFERENCES `room_type` (`room_code`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `room_status`
+--
+ALTER TABLE `room_status`
+  ADD CONSTRAINT `status_fk` FOREIGN KEY (`status`) REFERENCES `status` (`status`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `status_roomid_fk` FOREIGN KEY (`room_id`) REFERENCES `class_details` (`room_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `stocks`
