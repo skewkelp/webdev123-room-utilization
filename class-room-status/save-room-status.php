6cfbed2fbd093dc701da3f21860d4e13645eea4c
skewkelp <skewkelp@gmail.com>
2024-11-30 11:41:02 +0800

    sql file update to latest

--- class-room-status/save-room-status.php
+++ class-room-status/save-room-status.php
@@ -7,15 +7,21 @@ require_once('../classes/room-status.class.php');
 //(class-time)start-time, end-time, day
 //
 //this var refers to room_
-$name = $type = '';
-$nameErr = $typeErr = '';
+$room_id = $subject_id = $section_id = $teacher_assigned = $start_time = $end_time = $day_id = '';
+$room_idErr = $subject_idErr = $section_idErr = $teacher_assignedErr = $start_timeErr = $end_timeErr = $day_idErr = '';
 
 $roomObj = new Room();
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
-    $name = clean_input($_POST['room-name']);
-    $type = clean_input($_POST['room-type']);
+    $room_id = clean_input($_POST['room-id']);
+    $subject_id = clean_input($_POST['subject-id']);
+    $section_id = clean_input($_POST['section-id']);
+    $teacher_assigned = clean_input($_POST['teacher-assigned']);
+    $start_time = clean_input($_POST['start-time']);
+    $end_time = clean_input($_POST['end-time']);
+    $day_id = clean_input($_POST['day-id']);
+  
 
     if(empty($name)){
         $nameErr = 'Room name is required.';
--- room_utilization.sql
+++ room_utilization.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2024 at 10:13 PM
+-- Generation Time: Nov 30, 2024 at 04:40 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
